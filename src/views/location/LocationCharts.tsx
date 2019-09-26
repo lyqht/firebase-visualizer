9f75c6b982c2522bcd3bf995bc9080a6e3122204
lyqht <estee.tsw@gmail.com>
2019-09-26 16:05:30 +0800

    amended chart view

--- src/views/location/LocationCharts.tsx
+++ src/views/location/LocationCharts.tsx
@@ -1,62 +1,25 @@
 import React, { PureComponent } from "react";
-import { LocationProps } from "../LocationSection";
 import {
-    VictoryChart,
-    VictoryTheme,
     VictoryAxis,
+    VictoryBar,
+    VictoryChart,
     VictoryLabel,
-    VictoryBar
+    VictoryTheme
 } from "victory";
+import {
+    convertDataForBarChart,
+    CategoryData
+} from "../../helpers/dataFilterUtils";
+import { LocationProps } from "../LocationSection";
 import { CSSProperties } from "../styles";
 
 export class LocationCharts extends PureComponent<LocationProps> {
     render() {
         const { locationStats } = this.props;
-        const data = {
-            Families: [],
-            ActiveAging: [],
-            Preschool: [],
-            Others: []
-        };
-        let familiesIndex = 1;
-        let aaIndex = 1;
-        let othersIndex = 1;
-        Object.keys(locationStats).map(location => {
-            const locationCount = locationStats[location];
-            if (location.toLowerCase().includes("families")) {
-                data.Families.push({
-                    location,
-                    count: locationCount,
-                    index: familiesIndex
-                });
-                familiesIndex += 1;
-            } else if (
-                location.toLowerCase().includes("activeageing") ||
-                location.toLowerCase().includes("activeaging")
-            ) {
-                data.ActiveAging.push({
-                    location,
-                    count: locationCount,
-                    index: aaIndex
-                });
-            } else if (location.toLowerCase().includes("ecda")) {
-                data.Preschool.push({
-                    location,
-                    count: locationCount,
-                    index: data.Preschool.length + 1
-                });
-            } else {
-                data.Others.push({
-                    location,
-                    count: locationCount,
-                    index: othersIndex
-                });
-            }
-        });
-
+        const data: CategoryData = convertDataForBarChart(locationStats);
         return (
-            <div style={chartStyles.mainContainer}>
-                {Object.keys(data).map((section, index) => {
+            <div style={chartStyles.gridContainer}>
+                {Object.keys(data).map((section: string, index: number) => {
                     let locationNameTicks = [];
                     let locationCounts = [];
                     data[section].forEach(element => {
@@ -65,69 +28,132 @@ export class LocationCharts extends PureComponent<LocationProps> {
                     });
                     const numLocations = data[section].length;
                     const maxCount = Math.max(...locationCounts);
-                    return (
-                        <div key={index} style={chartStyles.gridContainer}>
-                            <VictoryChart
-                                theme={VictoryTheme.material}
-                                padding={{
-                                    top: 40,
-                                    bottom: 80,
-                                    left: 160,
-                                    right: 40
-                                }}
-                                style={{
-                                    parent: chartStyles.gridItem
-                                }}
-                                domainPadding={20}
-                            >
-                                <VictoryAxis
-                                    tickValues={[
-                                        ...Array(numLocations).keys()
-                                    ].slice(1)}
-                                    tickFormat={locationNameTicks}
-                                    tickLabelComponent={
-                                        <VictoryLabel style={{ fontSize: 8 }} />
-                                    }
-                                />
-                                <VictoryAxis
-                                    dependentAxis
-                                    tickValues={[...Array(maxCount).keys()]}
-                                    label={section}
-                                    axisLabelComponent={
-                                        <VictoryLabel dy={20} />
-                                    }
-                                    fixLabelOverlap
-                                />
-                                <VictoryBar
-                                    horizontal
-                                    data={data[section]}
-                                    x="index"
-                                    y="count"
-                                    labels={locationCounts}
-                                />
-                            </VictoryChart>
-                        </div>
-                    );
+                    if (numLocations < 8) {
+                        return Chart(
+                            Size.small,
+                            data,
+                            section,
+                            numLocations,
+                            locationNameTicks,
+                            locationCounts,
+                            maxCount
+                        );
+                    } else {
+                        return Chart(
+                            Size.big,
+                            data,
+                            section,
+                            numLocations,
+                            locationNameTicks,
+                            locationCounts,
+                            maxCount
+                        );
+                    }
                 })}
             </div>
         );
     }
 }
 
+enum Size {
+    small = "small",
+    big = "big"
+}
+
+const Chart = (
+    size: Size,
+    data: CategoryData,
+    section: string,
+    numLocations: number,
+    locationNameTicks: string[],
+    locationCounts: string[],
+    maxCount: number
+) => {
+    let gridItemContainerStyle = {};
+    let tickStyle = {};
+    let paddingStyle = {};
+    if (size === Size.small) {
+        gridItemContainerStyle = chartStyles.gridSmallItem;
+        tickStyle = chartStyles.bigText;
+        paddingStyle = {
+            top: 40,
+            bottom: 50,
+            left: 120,
+            right: 40
+        };
+    } else {
+        gridItemContainerStyle = chartStyles.gridBigItem;
+        tickStyle = chartStyles.smallText;
+        paddingStyle = {
+            top: 40,
+            bottom: 50,
+            left: 120,
+            right: 40
+        };
+    }
+    return (
+        <div style={gridItemContainerStyle}>
+            <VictoryChart
+                theme={VictoryTheme.material}
+                padding={paddingStyle}
+                style={{
+                    parent: gridItemContainerStyle
+                }}
+                domainPadding={20}
+            >
+                <VictoryAxis
+                    tickValues={[...Array(numLocations).keys()].slice(1)}
+                    tickFormat={locationNameTicks}
+                    tickLabelComponent={<VictoryLabel style={tickStyle} />}
+                />
+                <VictoryAxis
+                    dependentAxis
+                    tickValues={[...Array(maxCount).keys()]}
+                    label={section}
+                    tickLabelComponent={
+                        <VictoryLabel style={{ display: "none" }} />
+                    }
+                    axisLabelComponent={<VictoryLabel dy={20} />}
+                    fixLabelOverlap
+                />
+                <VictoryBar
+                    horizontal
+                    data={data[section]}
+                    x="index"
+                    y="count"
+                    labels={locationCounts}
+                    labelComponent={<VictoryLabel style={tickStyle} />}
+                />
+            </VictoryChart>
+        </div>
+    );
+};
+
 const chartStyles: CSSProperties = {
-    mainContainer: {
+    gridContainer: {
         display: "grid",
         justifyContent: "center",
-        gridTemplateColumns: "50% 50%",
-        gridTemplateRows: "auto",
+        gridTemplateColumns: "1fr 1fr 1fr",
+        gridTemplateRows: "300px 300px",
         margin: "auto",
-        gridGap: "3%"
+        height: "calc(100vh - 10px)"
     },
-    gridItem: {
+    gridSmallItem: {
         maxWidth: "100%",
         maxHeight: "100%"
     },
-    gridContainer: {
+    smallText: {
+        fontSize: "8"
+    },
+    bigText: {
+        fontSize: "12"
+    },
+    bigPadding: {},
+    gridBigItem: {
+        gridColumn: "span 3",
+        maxWidth: "100%"
+    },
+    gridItemContainer: {
         maxWidth: "100%",
         maxHeight: "100%"
     }
